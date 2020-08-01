ea2c2619ef0a4bb9ab09402706172a2708d1900c
Adam Brady <adam@gamemodeone.com>
2020-08-01 23:36:23 +0100

    Add collision detection for planet movement

--- Assets/Scripts/PlanetController.cs
+++ Assets/Scripts/PlanetController.cs
@@ -20,12 +20,13 @@ public class PlanetController : MonoBehaviour
 	public bool isMoveable = false;
     public bool isAnti = false;
 	public float mass = 0;
-    public float scale = 0;
+    public float scale = 1;
 
     public Sprite normalSprite;
     public Sprite antiSprite;
 
     private SpriteRenderer spriteRenderer;
+    private float radius;
 
     private List<MassLevel> massData = new List<MassLevel>()
     {
@@ -38,18 +39,6 @@ public class PlanetController : MonoBehaviour
     void Awake()
     {
         spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
-    }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 
     public void ChangeSize()
@@ -88,6 +77,49 @@ public class PlanetController : MonoBehaviour
 
     public void UpdatePosition(Vector3 pos)
     {
-        gameObject.transform.position = pos;
+        if(!WillCollideWithAny(pos))
+        {
+            gameObject.transform.position = pos;
+        }
+        else
+        {
+
+        }
+    }
+
+    public bool WillCollide(Vector3 posA, GameObject objB)
+    {
+        Vector3 posB = objB.transform.position;
+
+        float distance = Vector3.Distance(posA, posB);
+
+        // Collision Radius is scale * 0.8
+        float radii = scale * 0.8f + objB.GetComponent<CircleCollider2D>().radius;
+
+        return distance < radii;
+    }
+
+    public bool WillCollideWithAny(Vector3 newPos)
+    {
+        foreach(GameObject planet in LevelManager.instance.planets)
+        {
+            if(planet.GetInstanceID() != gameObject.GetInstanceID())
+            {
+                if(WillCollide(newPos, planet))
+                {
+                    return true;
+                }
+            }
+        }
+
+        foreach(GameObject star in LevelManager.instance.stars)
+        {
+            if(WillCollide(newPos, star))
+            {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
